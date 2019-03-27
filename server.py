c92fe1c0a9f787cdfac7f0447cf04a6bea6203c3
sano91 <vitaquant@gmail.com>
2019-03-27 01:32:01 +0100

    home page and main page ara upgraded, in main.html, useing bootstrap 4. navbar is done

--- server.py
+++ server.py
@@ -12,6 +12,9 @@ app.secret_key = b'420hokik420/'
 def home():
     return render_template('home.html')
 
+@app.route('/main')
+def main():
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run(
